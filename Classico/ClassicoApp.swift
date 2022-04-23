e323b430b99125d54d32616c0d6b16a8bdb1280a
Thibaut Izard <thibaut.izard@gmail.com>
2022-04-23 20:18:35 +0200

    Initial Commit

---
+++ Classico/ClassicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassicoApp.swift
+//  Classico
+//
+//  Created by Thibaut Izard on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClassicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClassicoUITests/ClassicoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ClassicoUITestsLaunchTests.swift
+//  ClassicoUITests
+//
+//  Created by Thibaut Izard on 23/04/2022.
+//
+
+import XCTest
+
+class ClassicoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
